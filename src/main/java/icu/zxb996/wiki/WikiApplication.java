82953bf47588e8a211869407769ed8415de27cbf
Gavin Zhang <zxb_worky@163.com>
2022-07-04 15:33:11 +0800

    2-2, init

---
+++ src/main/java/icu/zxb996/wiki/WikiApplication.java
@@ -0,0 +1,13 @@
+package icu.zxb996.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/zxb996/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.zxb996.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
