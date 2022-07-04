b11cd37049fb02855e036229d3966ad97dbf3726
Gavin Zhang <zxb_worky@163.com>
2022-07-04 18:43:18 +0800

    2-6, 增加ComponentScan来管理启动类扫包

--- src/main/java/icu/zxb996/wiki/config/WikiApplication.java
+++ src/main/java/icu/zxb996/wiki/config/WikiApplication.java
@@ -1,12 +1,14 @@
-package icu.zxb996.wiki;
+package icu.zxb996.wiki.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author gavin
  */
+@ComponentScan("icu.zxb996")
 @SpringBootApplication
 @Slf4j
 public class WikiApplication {
