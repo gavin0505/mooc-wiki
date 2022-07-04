b0dbd0d885c0852ef40e8eb9cc0394bd8f186046
Gavin Zhang <zxb_worky@163.com>
2022-07-04 17:46:50 +0800

    2-5, 增加logback配置文件

--- src/main/java/icu/zxb996/wiki/WikiApplication.java
+++ src/main/java/icu/zxb996/wiki/WikiApplication.java
@@ -3,6 +3,9 @@ package icu.zxb996.wiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author gavin
+ */
 @SpringBootApplication
 public class WikiApplication {
 
