e4e673bdea55891b0911e1eac500932633964886
Gavin Zhang <zxb_worky@163.com>
2022-07-04 18:07:29 +0800

    2-5, 增加Slf4j

--- src/main/java/icu/zxb996/wiki/WikiApplication.java
+++ src/main/java/icu/zxb996/wiki/WikiApplication.java
@@ -1,5 +1,6 @@
 package icu.zxb996.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author gavin
  */
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
+        log.info("启动成功！");
     }
 
 }
